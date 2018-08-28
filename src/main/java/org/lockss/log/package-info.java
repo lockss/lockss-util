318a7af92f56ed9c811e1a3cb458adfec00bdbae
Tom Lipkis <tal@pss.com>
2018-08-28 13:28:42 -0700

    pacakge doc

--- src/main/java/org/lockss/log/package-info.java
+++ src/main/java/org/lockss/log/package-info.java
@@ -1,11 +1,106 @@
-The classes in this package, together with several resource files extend
-log4j2 to provide compatibility with the legacy LOCKSS logger:
+/**
+<pre>
 
-- Provide most features of legacy logger:
-  - Obtain Logger instances with Logger.getLogger()
-  - Logger api
-  - Configure log level via LOCKSS configuration
-  Does not support the old LogTargets.  Targets (Appenders) must be
+This package provides a framework for LOCKSS logging, built on top of
+log4j, as well as binary compatibility with the legacy LOCKSS logger.
+
+Configuration:
+
+Logs are controlled by a chain of checked-in log4j config files and
+optionally a local file, a System property, and LOCKSS config params.
+
+Log4j config files are loded in this order, with later settings overriding
+eariler settings.  The exact merge rules are at
+https://logging.apache.org/log4j/2.x/manual/configuration.html#CompositeConfiguration
+
+  - log4j2-lockss.xml (in lockss-util:src/main/resources/) contains
+    standard Logger and Appender setup.  No other project should contain a
+    file with this name.
+
+  - log4j2-lockss-test.xml (in lockss-util:src/test/resources/) is
+    used by the logger tests.  If you need special logger setup for tests
+    in some other project, put them in a file with this name in the
+    project's test/resources/ dir.
+
+  - log4j2-lockss-project.xml is intended for downstream project-wide
+    config.  It should be checked into the project's main/resources/ dir.
+
+  - If the environemt variable LOG4J_LOCKSS_CONFIG is set, the file it
+    points to be will be loaded last.  This is intended for user-specific
+    config.
+
+log4j2-lockss.xml defines several properties to make it easier for
+downstream configs to make certain changes without repeating entire Logger
+or Appender elements:
+
+  &lt;Properties&gt;
+    &lt;!-- Default log level --&gt;
+    &lt;Property name="root.level"&gt;INFO&lt;/Property&gt;
+
+    &lt;!-- Default log dir --&gt;
+    &lt;Property name="log.dir"&gt;logs&lt;/Property&gt;
+
+    &lt;!-- Default pattern for log output to console --&gt;
+    &lt;Property name="layout.console"&gt;
+      %ld{HH:mm:ss.SSS} [%t] %-5level %logger{36}: %msg%lex%n
+    &lt;/Property&gt;
+
+    &lt;!-- Default pattern for log output to console from org.lockss.**
+         loggers.  Uses simple logger name as we're familiar with these
+         names. --&gt;
+    &lt;Property name="layout.console.lockss"&gt;
+      %ld{HH:mm:ss.SSS} [%t] %-5level %logger{1}: %msg%lex%n
+    &lt;/Property&gt;
+
+    &lt;!-- Patterns for file output and lockss file output default to
+         corresponding console patterns. --&gt;
+    &lt;Property name="layout.file"&gt;
+      ${layout.console}
+    &lt;/Property&gt;
+    &lt;Property name="layout.file.lockss"&gt;
+      ${layout.console.lockss}
+    &lt;/Property&gt;
+
+  &lt;/Properties&gt;
+
+
+The sysprop org.lockss.defaultLogLevel (-Dloglevel= in maven) overrides the
+default log level set in the log4j config file(s).
+
+LOCKSS config params (in lockss.txt, lockss.opt, expert config, etc.) of
+the form org.lockss.log.&lt;name&gt;.level can be used to override the log levels
+set in the log4j config files.  &lt;name&gt; can be simple or fq.
+org.lockss.log.default.level sets the default (root) level (overrides
+sysprop).
+
+LOCKSS loggers created with Logger.getLogger() or Logger.getLogger(Class)
+now get fully qualified names.  Those created with Logger.getLogger(String)
+still have an unqualified name if that's what's supplied.  The default log
+layout includes only the last component so they'll all look the same.
+Their log level can be set using config params with either the unqualified
+name or the fq name.  Levels for all loggers (not just those obtained with
+Logger.getLogger() or L4JLogger.getLogger() can be set with LOCKSS config
+params
+
+
+Using loggers:
+
+New code should use the log4j API
+(https://logging.apache.org/log4j/2.x/log4j-api/apidocs/org/apache/logging/log4j/Logger.html),
+via the L4JLogger subclass which includes the additional LOCKSS log levels
+(siteError, siteWarning, debug2 and debug3 (same as trace).
+
+  private static L4JLogger log = L4JLogger.getLogger();
+  ...
+  log.debug2("msg: {}", arg);
+
+The constants L4JLevel.SITE_ERROR, SITE_WARNING, DEBUG2, and DEBUG3 are
+analogous to log4j's Level.XXX .
+
+
+Implementation:
+
+- Does not support the old LogTargets.  Targets (Appenders) must be
   configured using log4j directly.
 
 - Define log4j custom log levels corresponding to Debug2, Debug3,
@@ -27,9 +122,9 @@ log4j2 to provide compatibility with the legacy LOCKSS logger:
   - log4j2-lockss-util.xml has a standard Logger and Appender setup and
     should not be overridden.
 
-  - log4j2-lockss-logger-test.xml is used by the logger tests in this package.
-    main/resources/log4j2-logger-test.xml is empty; the logger setup needed
-    for the tests is in test/resources/log4j2-logger-test.xml, which gets
+  - log4j2-lockss-test.xml is used by the logger tests in this package.
+    main/resources/log4j2-lockss-test.xml is empty; the logger setup needed
+    for the tests is in test/resources/log4j2-lockss-test.xml, which gets
     loaded when the tests are running because appears on the classpath
     before the main classes/files.
 
@@ -58,7 +153,8 @@ log4j2 to provide compatibility with the legacy LOCKSS logger:
   %lex, which is similar to %ex but which suppresses the stack trace if
   neither the message level nor the logger's configured level satisfies
   stackTraceSeverity and stackTraceLevel, respectively.  The Logger's level
-  is obtained from the ThreadContextStack, where L4JLogger
+  is obtained from the ThreadContextStack, which L4JLogger pushes and pops
+  around log calls.
 
 - The plugin L4JDatePatternConverter is a PatternConverter bound to %ld,
   which outputs the date like %d and appends " (sim XXX)" when the LOCKSS
@@ -76,9 +172,9 @@ log4j2 to provide compatibility with the legacy LOCKSS logger:
     to call those methods the result of LogManager.getLogger() must be
     downcast to L4JLogger.  Convenience methods are provided to do that:
 
-      L4JLogger log = L4JLogger.getLogger("name");
-      L4JLogger log = L4JLogger.getLogger(Class);
-      L4JLogger log = L4JLogger.getLogger();
+      L4JLogger log = L4JLogger.getLogger();  // log name is caller's class name
+      L4JLogger log = L4JLogger.getLogger(Class);  // log name is class name
+      L4JLogger log = L4JLogger.getLogger("name"); // log name is "name"
 
       L4JLogger log = L4JLogger.cast(LogManager.getFormatterLogger());
 
@@ -134,3 +230,6 @@ log4j2 to provide compatibility with the legacy LOCKSS logger:
     main/resources).  That way, if the standard file name does get loaded
     by some mechanism we can't control, it will at least be ours.  This
     seems to work well enough with Spring Boot.
+</pre>
+*/
+package org.lockss.log;
