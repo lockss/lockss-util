8571b9cee7f55b32668e17bff67041f5c79a6f46
Daniel Vargas <dlvargas@stanford.edu>
2020-10-15 18:41:13 -0700

    More gracefully handle errors parsing the error response body

--- lockss-util-rest/src/main/java/org/lockss/util/rest/LockssResponseErrorHandler.java
+++ lockss-util-rest/src/main/java/org/lockss/util/rest/LockssResponseErrorHandler.java
@@ -1,7 +1,6 @@
 package org.lockss.util.rest;
 
-import java.util.*;
-import org.lockss.log.*;
+import org.lockss.log.L4JLogger;
 import org.lockss.util.rest.exception.LockssRestException;
 import org.lockss.util.rest.exception.LockssRestHttpException;
 import org.springframework.http.HttpHeaders;
@@ -9,10 +8,15 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.web.client.*;
+import org.springframework.web.client.DefaultResponseErrorHandler;
+import org.springframework.web.client.RequestCallback;
+import org.springframework.web.client.ResponseExtractor;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 import java.net.URI;
+import java.util.List;
+import java.util.Map;
 
 public class LockssResponseErrorHandler extends DefaultResponseErrorHandler {
   private static L4JLogger log = L4JLogger.getLogger();
@@ -20,7 +24,7 @@ public class LockssResponseErrorHandler extends DefaultResponseErrorHandler {
   private final List<HttpMessageConverter<?>> messageConverters;
 
   public LockssResponseErrorHandler(List<HttpMessageConverter<?>>
-				    messageConverters) {
+                                        messageConverters) {
     this.messageConverters = messageConverters;
   }
 
@@ -30,46 +34,54 @@ public class LockssResponseErrorHandler extends DefaultResponseErrorHandler {
     HttpHeaders responseHeaders = response.getHeaders();
 
     LockssRestHttpException lrhe = new LockssRestHttpException();
-    lrhe.setHttpStatus(statusCode);
 
     boolean handled = false;
+
     // Convert the response body to e Map if possible (presumably because
     // the response is json)
-    try {
-      for (HttpMessageConverter converter : messageConverters) {
-	if (converter.canRead(Map.class, responseHeaders.getContentType())) {
-	  Map mmm = (Map)converter.read(Map.class, response);
-	  log.debug2("Server error map: {}", mmm);
-	  lrhe.setServerErrorMap(mmm);
-	  if (mmm.containsKey("message")) {
-	    Object msg = mmm.get("message");
-	    if (msg instanceof String) {
-	      lrhe.setServerErrorMessage((String)msg);
-	    }
-	  }
-	  handled = true;
-	}
+    for (HttpMessageConverter converter : messageConverters) {
+      if (converter.canRead(Map.class, responseHeaders.getContentType())) {
+        try {
+          Map mmm = (Map) converter.read(Map.class, response);
+          log.debug2("Server error map: {}", mmm);
+          lrhe.setServerErrorMap(mmm);
+          if (mmm.containsKey("message")) {
+            Object msg = mmm.get("message");
+            if (msg instanceof String) {
+              lrhe.setServerErrorMessage((String) msg);
+            }
+          }
+          handled = true;
+          break;
+        } catch (Exception e) {
+          log.debug2("Failed to convert body of {} response to Map", statusCode, e);
+        }
       }
-      if (!handled) {
-	// Convert the response body to e String if possible (presumably because
-	// the response is text/*)
-	for (HttpMessageConverter converter : messageConverters) {
-	  if (converter.canRead(String.class,
-				responseHeaders.getContentType())) {
-	    String sss = (String)converter.read(String.class, response);
-	    log.debug2("Server error message: {}", sss);
-	    lrhe.setServerErrorMessage(sss);
-	    handled = true;
-	  }
-	}
-      }
-      if (!handled) {
-	log.debug("Error body not handled: {}",
-		  responseHeaders.getContentType());
-      }
-    } catch (Exception e) {
-      log.warn("Failed to convert body of {} response", statusCode, e);
     }
+
+    if (!handled) {
+      // Convert the response body to e String if possible (presumably because
+      // the response is text/*)
+      for (HttpMessageConverter converter : messageConverters) {
+        if (converter.canRead(String.class, responseHeaders.getContentType())) {
+          try {
+            String sss = (String) converter.read(String.class, response);
+            log.debug2("Server error message: {}", sss);
+            lrhe.setServerErrorMessage(sss);
+            handled = true;
+            break;
+          } catch (Exception e) {
+            log.debug2("Failed to convert body of {} response to String", statusCode, e);
+          }
+        }
+      }
+    }
+
+    if (!handled) {
+      log.debug("Error body not handled: {}", responseHeaders.getContentType());
+    }
+
+    lrhe.setHttpStatus(statusCode);
     lrhe.setHttpResponseHeaders(responseHeaders);
     lrhe.setMessage("FIXME");
 
