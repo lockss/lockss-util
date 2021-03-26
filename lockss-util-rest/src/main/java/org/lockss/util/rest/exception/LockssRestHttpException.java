3ecf72253f53fce3d0b4cbd42acb177629398852
Daniel Vargas <dlvargas@stanford.edu>
2021-03-26 09:39:23 -0700

    Introduced ServerErrorType enum, field, and getters and setters; modified LRHE to take a RestResponseError instead of Map

--- lockss-util-rest/src/main/java/org/lockss/util/rest/exception/LockssRestHttpException.java
+++ lockss-util-rest/src/main/java/org/lockss/util/rest/exception/LockssRestHttpException.java
@@ -1,6 +1,6 @@
 /*
 
- Copyright (c) 2019 Board of Trustees of Leland Stanford Jr. University,
+ Copyright (c) 2019-2021 Board of Trustees of Leland Stanford Jr. University,
  all rights reserved.
 
  Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -27,21 +27,81 @@
  */
 package org.lockss.util.rest.exception;
 
-import java.util.Map;
+import org.lockss.log.L4JLogger;
+import org.lockss.util.rest.RestResponseErrorBody;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpInputMessage;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.client.RestClientResponseException;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+/**
+ * A subclass of {@link LockssRestException} representing HTTP error responses returned
+ * by LOCKSS Spring Boot applications.
+ */
 public class LockssRestHttpException extends LockssRestException {
   private static final long serialVersionUID = -4151454747192096531L;
+  private static L4JLogger log = L4JLogger.getLogger();
 
+  /**
+   * HTTP response status of error.
+   */
   private HttpStatus httpStatus;
-  private String httpStatusMessage;
-  private HttpHeaders responseHeaders;
-  private String srvErrMessage;
-  private Map srvErrMap;
 
-  // Temporary
-  private String contextMessage;
+  /**
+   * Error response headers.
+   */
+  private HttpHeaders responseHeaders;
+
+  /**
+   * Server error message.
+   */
+  private String serverErrorMessage;
+
+  /**
+   * A {@link RestResponseErrorBody.RestResponseError} representing the HTTP response.
+   *
+   */
+  private RestResponseErrorBody.RestResponseError restResponseError;
+
+  /**
+   * Type of error experienced by the server (for 5xx series of errors). See {@link ServerErrorType}.
+   */
+  private ServerErrorType serverErrorType = ServerErrorType.NONE;
+
+  /**
+   * Client error message from RestTemplate call.
+   */
+  private String clientErrorMessage;
+
+  /**
+   * Holds the exception that was thrown if there was an error parsing the error response body.
+   */
+  private Exception parseException;
+
+  /**
+   * Enum help distinguish types of 5xx series HTTP errors experienced by LOCKSS Spring Boot applications.
+   *
+   * <ul>
+   *   <li>{@code NONE}: Not a server error.</li>
+   *   <li>{@code UNSPECIFIED_ERROR}: Default if not specified by REST application.</li>
+   *   <li>{@code APPLICATION_ERROR}: Errors which occurred with the Spring Boot application itself.</li>
+   *   <li>{@code DATA_ERROR}: Errors which occurred because of an irrecoverable problem with the data.</li>
+   * </ul>
+   */
+  public enum ServerErrorType {
+    NONE,
+    UNSPECIFIED_ERROR,
+    APPLICATION_ERROR,
+    DATA_ERROR,
+  }
 
   /**
    * Default constructor.
@@ -110,7 +170,7 @@ public class LockssRestHttpException extends LockssRestException {
   /**
    * Saves the HTTP response headers.
    * 
-   * @param httpStatusMessage
+   * @param responseHeaders
    *          An HttpHeaders with the HTTP response headers.
    * @return a LockssRestHttpException with this object.
    */
@@ -126,7 +186,7 @@ public class LockssRestHttpException extends LockssRestException {
    * @return Server error message, or null
    */
   public String getServerErrorMessage() {
-    return srvErrMessage;
+    return serverErrorMessage;
   }
 
   /**
@@ -136,30 +196,155 @@ public class LockssRestHttpException extends LockssRestException {
    * @return a LockssRestHttpException with this object.
    */
   public LockssRestHttpException setServerErrorMessage(String errMessage) {
-    this.srvErrMessage = errMessage;
+    this.serverErrorMessage = errMessage;
     return this;
   }
 
   /**
-   * Return the server error Map, if any.
+   * Returns the error response parsing exception, if any.
    *
-   * @return Map containing error details, or null
+   * @return An
    */
-  public Map getServerErrorMap() {
-    return srvErrMap;
+  public Exception getParseException() {
+    return parseException;
+  }
+
+  public void setParseException(Exception parseException) {
+    this.parseException = parseException;
   }
 
   /**
-   * Set the server error Map
+   * Return a {@link org.lockss.util.rest.RestResponseErrorBody.RestResponseError} containing
+   * the response error details, if any.
    *
-   * @param errMap Explanatory Map returned by the server
-   * @return a LockssRestHttpException with this object.
+   * @return
    */
-  public LockssRestHttpException setServerErrorMap(Map errMap) {
-    this.srvErrMap = errMap;
+  public RestResponseErrorBody.RestResponseError getRestResponseError() {
+    return restResponseError;
+  }
+
+  public LockssRestHttpException setRestResponseError(RestResponseErrorBody.RestResponseError error) {
+    this.restResponseError = error;
     return this;
   }
 
+  /**
+   * Returns the type of server error reported by the server, if any.
+   *
+   * @return The {@link ServerErrorType} reported by the server.
+   */
+  public ServerErrorType getServerErrorType() {
+    return serverErrorType;
+  }
+
+  /**
+   * Sets the server error type.
+   *
+   * @param serverErrorType A {@link ServerErrorType}.
+   */
+  public void setServerErrorType(ServerErrorType serverErrorType) {
+    this.serverErrorType = serverErrorType;
+  }
+
+  /**
+   * Transforms a {@link RestClientResponseException} to a new {@link LockssRestHttpException} using the first
+   * {@link HttpMessageConverter} capable of reading the error response body, from a list of provided
+   * {@link HttpMessageConverter}s.
+   *
+   * @param e1                The {@link RestClientResponseException} to transform.
+   * @param messageConverters A {@link List <HttpMessageConverter>} to try to read the error response body with.
+   * @return A {@link LockssRestHttpException} representing the {@link RestClientResponseException}.
+   */
+  public static LockssRestHttpException fromRestClientResponseException(
+      RestClientResponseException e1, List<HttpMessageConverter<?>> messageConverters) {
+
+    log.debug2("errorResponseBody = {}", e1.getResponseBodyAsString());
+
+    // LockssRestHttpException to populate and return
+    LockssRestHttpException lrhe = new LockssRestHttpException();
+
+    // Populate LockssRestHttpException with base information
+    lrhe.setHttpStatus(HttpStatus.valueOf(e1.getRawStatusCode()));
+    lrhe.setHttpResponseHeaders(e1.getResponseHeaders());
+//    lrhe.setServerErrorMessage(e1.getMessage());
+
+    boolean additionalDetailsAdded = false;
+
+    // Iterate over HTTP message converters to parse error response body (both 4xx and 5xx errors)
+    for (HttpMessageConverter converter : messageConverters) {
+      // Check if this HTTP message converter can deserialize to RestResponseErrorBody
+      if (converter.canRead(
+          RestResponseErrorBody.RestResponseError.class,
+          e1.getResponseHeaders().getContentType())) {
+
+        try {
+          // Attempt to deserialize the input into a RestResponseError object
+          RestResponseErrorBody.RestResponseError responseError =
+              (RestResponseErrorBody.RestResponseError) converter.read(
+                  RestResponseErrorBody.RestResponseError.class,
+                  // ByteArrayHttpInputMessage is used here instead of the provided ClientHttpResponse to avoid
+                  // problems of successive HttpMessageConverters reading from a possibly exhausted InputStream
+                  // in ClientHttpResponse
+                  new ByteArrayHttpInputMessage(e1.getResponseHeaders(), e1.getResponseBodyAsByteArray())
+              );
+
+          // Create and populate a new LockssRestHttpException
+          lrhe.setRestResponseError(responseError);
+          lrhe.setServerErrorMessage(responseError.getMessage());
+          lrhe.setServerErrorType(responseError.getServerErrorType());
+
+          additionalDetailsAdded = true;
+
+        } catch (IOException | HttpMessageNotReadableException e2) {
+          // An error occurred while reading or parsing the error response body
+          log.error("An error occurred while reading or parsing the error response body with {}",
+              converter.getClass().getSimpleName(), e2);
+
+          // Save the parsing error in the LRHE
+          lrhe.setParseException(e2);
+
+          // Abort error response body parsing and allow method to return a minimally populated LRHE
+          break;
+        }
+      }
+    }
+
+    if (!additionalDetailsAdded) {
+      lrhe.setServerErrorMessage(e1.getResponseBodyAsString());
+    }
+
+    return lrhe;
+  }
+
+  /**
+   * Byte array implementation of {@link HttpInputMessage}.
+   * <p>
+   * This was written as a workaround for successive {@link HttpMessageConverter}s from reading from the same and
+   * possibly exhausted {@link InputStream} in {@link ClientHttpResponse}.
+   * <p>
+   * TODO: Move to lockss-util? Or lockss-spring-bundle?
+   */
+  public static class ByteArrayHttpInputMessage implements HttpInputMessage {
+    private final HttpHeaders headers;
+    private final byte[] inputMessageBytes;
+
+    public ByteArrayHttpInputMessage(HttpHeaders headers, byte[] inputMessageBytes) {
+      this.headers = headers;
+      this.inputMessageBytes = inputMessageBytes;
+    }
+
+    @Override
+    public InputStream getBody() throws IOException {
+      return new ByteArrayInputStream(inputMessageBytes);
+    }
+
+    @Override
+    public HttpHeaders getHeaders() {
+      return headers;
+    }
+  }
+
+  @Override
   public String getMessage() {
     StringBuilder sb = new StringBuilder();
     sb.append(getHttpStatusCode());
@@ -167,7 +352,7 @@ public class LockssRestHttpException extends LockssRestException {
     sb.append(getHttpStatusMessage());
 
 //    String message = super.getMessage();
-    String message = contextMessage;
+    String message = clientErrorMessage;
 
     if (message != null) {
       sb.append(": ");
@@ -176,8 +361,13 @@ public class LockssRestHttpException extends LockssRestException {
     return sb.toString();
   }
 
-  public void setMessage(String message) {
-    contextMessage = message;
+  /**
+   * Sets the client error message, if any.
+   *
+   * @param message A {@link String} containing the client error message.
+   */
+  public void setClientErrorMessage(String message) {
+    clientErrorMessage = message;
   }
 }
 
