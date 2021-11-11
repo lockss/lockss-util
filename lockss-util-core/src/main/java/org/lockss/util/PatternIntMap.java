283487fcbc1075beb0e7826088e37ad2a2b9f23f
Tom Lipkis <tal@pss.com>
2021-11-11 08:41:30 -0800

    Revert "Upgrade to Commons Lang4, Collections4"
    This reverts commit 58d247a7026a9279d69003e8b7ccb16d074a03a9.

--- lockss-util-core/src/main/java/org/lockss/util/PatternIntMap.java
+++ lockss-util-core/src/main/java/org/lockss/util/PatternIntMap.java
@@ -32,7 +32,7 @@ import java.util.*;
 import java.text.*;
 import java.util.regex.*;
 import org.lockss.log.*;
-import org.apache.commons.collections4.map.*;
+import org.apache.commons.collections.map.*;
 
 /** "Map" strings to integers, where the keys are patterns against which
  * the strings are matched.  The patterns are ordered; the value associated
