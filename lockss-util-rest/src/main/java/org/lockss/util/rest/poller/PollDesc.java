802cc6204df9b74a60f12fd5ade4f136e9ae4eb5
Claire Griffin <clairetg@stanford.edu>
2024-02-14 11:37:54 -0800

    clean up unused import

--- lockss-util-rest/src/main/java/org/lockss/util/rest/poller/PollDesc.java
+++ lockss-util-rest/src/main/java/org/lockss/util/rest/poller/PollDesc.java
@@ -36,7 +36,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 import io.swagger.v3.oas.annotations.media.Schema;
-import org.lockss.laaws.poller.model.CachedUriSetSpec;
 import org.springframework.validation.annotation.Validated;
 import javax.validation.Valid;
 import javax.validation.constraints.*;
