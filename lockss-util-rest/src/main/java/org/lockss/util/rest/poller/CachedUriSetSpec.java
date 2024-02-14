52873db11848f209bbf2e43c7988495d83474e9f
dlvargas <dlvargas@stanford.edu>
2024-02-13 17:33:28 -0800

    Re-generated missed models using OpenAPI3 spec

--- lockss-util-rest/src/main/java/org/lockss/util/rest/poller/CachedUriSetSpec.java
+++ lockss-util-rest/src/main/java/org/lockss/util/rest/poller/CachedUriSetSpec.java
@@ -1,32 +1,32 @@
 /*
 
-Copyright (c) 2020 Board of Trustees of Leland Stanford Jr. University,
-all rights reserved.
+Copyright (c) 2000-2024, Board of Trustees of Leland Stanford Jr. University
 
-Redistribution and use in source and binary forms, with or without modification,
-are permitted provided that the following conditions are met:
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
 
-1. Redistributions of source code must retain the above copyright notice, this
-list of conditions and the following disclaimer.
+1. Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
 
 2. Redistributions in binary form must reproduce the above copyright notice,
-this list of conditions and the following disclaimer in the documentation and/or
-other materials provided with the distribution.
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
 
 3. Neither the name of the copyright holder nor the names of its contributors
 may be used to endorse or promote products derived from this software without
 specific prior written permission.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
-ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
-ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
 
  */
 package org.lockss.util.rest.poller;
@@ -34,8 +34,7 @@ package org.lockss.util.rest.poller;
 import java.util.Objects;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonCreator;
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.validation.annotation.Validated;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.*;
@@ -43,8 +42,10 @@ import jakarta.validation.constraints.*;
 /**
  * A set of urls bounded by  upper and lower restraints.  If lower &#x3D; \&quot;.\&quot; this is a singleContentNode. If lower is null then start at the root url, if upper is null end with the last.
  */
-@ApiModel(description = "A set of urls bounded by  upper and lower restraints.  If lower = \".\" this is a singleContentNode. If lower is null then start at the root url, if upper is null end with the last.")
+@Schema(description = "A set of urls bounded by  upper and lower restraints.  If lower = \".\" this is a singleContentNode. If lower is null then start at the root url, if upper is null end with the last.")
 @Validated
+
+
 
 public class CachedUriSetSpec   {
   @JsonProperty("urlPrefix")
@@ -64,12 +65,11 @@ public class CachedUriSetSpec   {
   /**
    * The base which roots the lower and upper bound
    * @return urlPrefix
-  **/
-  @ApiModelProperty(required = true, value = "The base which roots the lower and upper bound")
-  @NotNull
+   **/
+  @Schema(required = true, description = "The base which roots the lower and upper bound")
+      @NotNull
 
-
-  public String getUrlPrefix() {
+    public String getUrlPrefix() {
     return urlPrefix;
   }
 
@@ -85,11 +85,10 @@ public class CachedUriSetSpec   {
   /**
    * lower bound of the prefix range, inclusive.
    * @return lowerBound
-  **/
-  @ApiModelProperty(value = "lower bound of the prefix range, inclusive.")
-
-
-  public String getLowerBound() {
+   **/
+  @Schema(description = "lower bound of the prefix range, inclusive.")
+  
+    public String getLowerBound() {
     return lowerBound;
   }
 
@@ -105,11 +104,10 @@ public class CachedUriSetSpec   {
   /**
    * upper bound of prefix range, inclusive.
    * @return upperBound
-  **/
-  @ApiModelProperty(value = "upper bound of prefix range, inclusive.")
-
-
-  public String getUpperBound() {
+   **/
+  @Schema(description = "upper bound of prefix range, inclusive.")
+  
+    public String getUpperBound() {
     return upperBound;
   }
 
@@ -160,4 +158,3 @@ public class CachedUriSetSpec   {
     return o.toString().replace("\n", "\n    ");
   }
 }
-
